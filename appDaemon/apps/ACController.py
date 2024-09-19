bdba410a4b5663ea1f6159b8265443803564a0ab
root <root@core-ssh.local.hass.io>
2024-09-19 20:04:56 +0300

    Smaller modifications of the AC automation script

--- appDaemon/apps/ACController.py
+++ appDaemon/apps/ACController.py
@@ -28,11 +28,34 @@ class ACController(hass.Hass):
   day_transfer_charge           = 3.87      # Grid transfer cost in cent/kWh from 07 - 22
   night_transfer_charge         = 1.31      # Grid transfer cost in cent/kWh from 22 - 07
 
-  state_update_timer            = 60        # How many seconds between two state updates
+  state_update_timer            = 5        # How many seconds between two state updates
+
+  #manual_override_end_time      = datetime.now()  # Defines when manual override will stop
+  #manual_override_target_temp   = 23              # What temperature to set when manual override is active
+
 
   entity_id_climate_control = "climate.153931628243065_climate"
   entity_id_weather_forecast = "weather.forecast_home"
   entity_id_nordpool_sensor  = "sensor.nordpool_kwh_fi_eur_3_10_024"
+
+
+  target_room_min_temperature_id = "input_number.target_room_min_temperature"
+  target_room_temperature_id = "input_number.target_room_temperature"
+  target_room_max_temperature_id = "input_number.target_room_max_temperature"
+  
+  min_mean_price_multiplier_id = "input_number.min_mean_price_multiplier"
+  max_mean_price_multiplier_id = "input_number.max_mean_price_multiplier"
+  
+  min_absolute_price_id = "input_number.min_absolute_price"
+  max_absolute_price_id = "input_number.max_absolute_price"
+  
+  min_state_change_time_id = "input_number.min_state_change_time"
+  ignore_change_time_temp_diff_id = "input_number.ignore_change_time_temp_diff"
+  
+  day_transfer_charge_id = "input_number.day_transfer_charge"
+  night_transfer_charge_id = "input_number.night_transfer_charge"
+
+  #manual_override_end_time_id = "input_datetime.manual_override_end_time"
 
   # TODO: AC energy multiplier curve based on outside and inside temp difference. AC is more effective during hours when outside temp
   #       is higher, which can be used to make a better price estimation when we move from pure electricity pricing calculation to
@@ -49,12 +72,187 @@ class ACController(hass.Hass):
   def initialize(self):
     # Define all variables inside the instance of this class
     self.last_state_change_time = datetime.now() - timedelta(seconds=self.min_state_change_time+1)
+    self.initialize_all_parameters()
+    self.update_internal_parameters()
 
     # Define the starting time
     # now = datetime.datetime.now()
 
     # Schedule the function to run every state_update_timer seconds
     self.run_every(self.control_climate, "now", self.state_update_timer)
+
+  def create_input_date(self, entity_id, name):
+    # Check if the entity already exists
+    entity_state = self.get_state(entity_id)
+    # entity_state = None
+    
+    if entity_state is None:
+      # Entity doesn't exist, so create it with the initial value
+      self.log(f"Creating {entity_id} with initial value")
+      now = datetime.now()
+      self.set_state(entity_id, state="", attributes={
+          "year": now.year,
+          "month": now.month,
+          "day": now.day,
+          "hour": now.hour,
+          "second": now.second,
+          "friendly_name": name
+      })
+
+  def create_input_number(self, entity_id, name, initial, min_value, max_value, step):
+    # Check if the entity already exists
+    entity_state = self.get_state(entity_id)
+    # entity_state = None
+    
+    if entity_state is None:
+      # Entity doesn't exist, so create it with the initial value
+      self.log(f"Creating {entity_id} with initial value {initial}")
+      self.set_state(entity_id, state=initial, attributes={
+          "min": min_value,
+          "max": max_value,
+          "step": step,
+          "mode": "slider",
+          #"mode": "slider-entity-row",
+          "friendly_name": name
+      })
+    #else:
+      # Entity exists, don't overwrite it
+      #self.log(f"{entity_id} already exists with value {entity_state}")
+
+  def initialize_all_parameters(self):
+    # Dynamically create or set default values for input_number entities only if they don't exist
+    # self.log(f"Update all parameters")
+    self.create_input_number(self.target_room_min_temperature_id, "Min Room Temperature", 18, 10, 30, 0.5)
+    self.create_input_number(self.target_room_temperature_id, "Target Room Temperature", 23, 10, 30, 0.5)
+    self.create_input_number(self.target_room_max_temperature_id, "Max Room Temperature", 24, 10, 30, 0.5)
+    
+    self.create_input_number(self.min_mean_price_multiplier_id, "Min Mean Price Multiplier", 0.5, 0.1, 2.0, 0.1)
+    self.create_input_number(self.max_mean_price_multiplier_id, "Max Mean Price Multiplier", 1.5, 0.1, 2.0, 0.1)
+    
+    self.create_input_number(self.min_absolute_price_id, "Min Absolute Price", 5.0, 0.0, 100.0, 0.5)
+    self.create_input_number(self.max_absolute_price_id, "Max Absolute Price", 30.0, 0.0, 100.0, 0.5)
+    
+    self.create_input_number(self.min_state_change_time_id, "Min State Change Time (sec)", 1800, 0, 3600, 60)
+    self.create_input_number(self.ignore_change_time_temp_diff_id, "Temp Diff to Ignore Time", 2, 0, 10, 0.5)
+    
+    self.create_input_number(self.day_transfer_charge_id, "Electricity Grid Day Transfer Charge", 3.87, 0.0, 10.0, 0.1)
+    self.create_input_number(self.night_transfer_charge_id, "Electricity Grid Night Transfer Charge", 1.31, 0.0, 10.0, 0.1)
+
+    # self.create_input_date(self.manual_override_end_time_id, "Manual control end time")
+
+    # manual_override_end_time
+    #manual_override_target_temp   
+
+    self.listen_event(self.change_state, event = "call_service")
+
+    # Set up state listeners (callbacks) for when these values change
+    self.listen_state(self.input_number_changed, self.target_room_min_temperature_id)
+    self.listen_state(self.input_number_changed, self.target_room_temperature_id)
+    self.listen_state(self.input_number_changed, self.target_room_max_temperature_id)
+    
+    self.listen_state(self.input_number_changed, self.min_mean_price_multiplier_id)
+    self.listen_state(self.input_number_changed, self.max_mean_price_multiplier_id)
+    
+    self.listen_state(self.input_number_changed, self.min_absolute_price_id)
+    self.listen_state(self.input_number_changed, self.max_absolute_price_id)
+    
+    self.listen_state(self.input_number_changed, self.min_state_change_time_id)
+    self.listen_state(self.input_number_changed, self.ignore_change_time_temp_diff_id)
+    
+    self.listen_state(self.input_number_changed, self.day_transfer_charge_id)
+    self.listen_state(self.input_number_changed, self.night_transfer_charge_id)
+
+    # self.listen_state(self.input_datetime_changed, self.manual_override_end_time_id)
+
+
+  def update_internal_parameters(self):
+    # Update our local properties for ease of use
+    self.target_room_min_temperature = float(self.get_state(self.target_room_min_temperature_id))
+    self.target_room_temperature = float(self.get_state(self.target_room_temperature_id))
+    self.target_room_max_temperature = float(self.get_state(self.target_room_max_temperature_id))
+
+    self.min_mean_price_multiplier = float(self.get_state(self.min_mean_price_multiplier_id))
+    self.max_mean_price_multiplier = float(self.get_state(self.max_mean_price_multiplier_id))
+
+    self.min_absolute_price = float(self.get_state(self.min_absolute_price_id))
+    self.max_absolute_price = float(self.get_state(self.max_absolute_price_id))
+
+    self.min_state_change_time = int(self.get_state(self.min_state_change_time_id))
+    self.ignore_change_time_temp_diff = float(self.get_state(self.ignore_change_time_temp_diff_id))
+
+    self.day_transfer_charge = float(self.get_state(self.day_transfer_charge_id))
+    self.night_transfer_charge = float(self.get_state(self.night_transfer_charge_id))
+
+  def change_state(self,event_name,data, kwargs):
+    entity_id = data["service_data"]["entity_id"]
+    new_value = data["service_data"].get("value")
+
+    # self.log(data)
+    
+    if entity_id == self.target_room_min_temperature_id:
+        self.set_state(self.target_room_min_temperature_id, state=new_value)
+        
+    elif entity_id == self.target_room_temperature_id:
+        self.set_state(self.target_room_temperature_id, state=new_value)
+        
+    elif entity_id == self.target_room_max_temperature_id:
+        self.set_state(self.target_room_max_temperature_id, state=new_value)
+        
+    elif entity_id == self.min_mean_price_multiplier_id:
+        self.set_state(self.min_mean_price_multiplier_id, state=new_value)
+        
+    elif entity_id == self.max_mean_price_multiplier_id:
+        self.set_state(self.max_mean_price_multiplier_id, state=new_value)
+        
+    elif entity_id == self.min_absolute_price_id:
+        self.set_state(self.min_absolute_price_id, state=new_value)
+        
+    elif entity_id == self.max_absolute_price_id:
+        self.set_state(self.max_absolute_price_id, state=new_value)
+        
+    elif entity_id == self.min_state_change_time_id:
+        self.set_state(self.min_state_change_time_id, state=new_value)
+        
+    elif entity_id == self.ignore_change_time_temp_diff_id:
+        self.set_state(self.ignore_change_time_temp_diff_id, state=new_value)
+        
+    elif entity_id == self.day_transfer_charge_id:
+        self.set_state(self.day_transfer_charge_id, state=new_value)
+        
+    elif entity_id == self.night_transfer_charge_id:
+        self.set_state(self.night_transfer_charge_id, state=new_value)
+
+    #elif entity_id == self.manual_override_end_time_id:
+    #    new_time = data["service_data"].get("time")
+    #    self.set_state(self.manual_override_end_time_id, state=new_value)
+
+    #if(data["service"] == "turn_off" and data["service_data"]["entity_id"] == self.input_boolean):
+    #    self.log(self.input_boolean + "switched off")
+    #    self.set_state(self.input_boolean, state = "off")
+    #if(data["service"] == "turn_on" and data["service_data"]["entity_id"] == self.input_boolean):
+    #    self.log(self.input_boolean + "switched on")
+    #    self.set_state(self.input_boolean, state = "on")
+
+
+  def input_number_changed(self, entity, attribute, old, new, kwargs):
+      # Log whenever an input_number value changes
+      self.log(f"{entity} changed from {old} to {new}")
+      
+      # Update the internal values after the change
+      self.update_internal_parameters()
+
+      # Allow immediate AC state update
+      self.last_state_change_time = datetime.now() - timedelta(seconds=self.min_state_change_time+1)
+
+  def input_datetime_changed(self, entity, attribute, old, new, kwargs):
+      # Log whenever an input_datetime value changes
+      self.log(f"{entity} changed from {old} to {new}")
+      
+      # Update the internal values after the change
+      self.update_internal_parameters()
+
+      # Allow immediate AC state update
+      self.last_state_change_time = datetime.now() - timedelta(seconds=self.min_state_change_time+1)
 
 
   def normalize_value(self, value_now, max_value, min_value):
@@ -121,8 +319,6 @@ class ACController(hass.Hass):
   # Calculates the target temperature for this hour based on readings and price
   def calculate_target_temperature(self):
     inside_temperature = self.get_state(self.entity_id_climate_control, attribute="current_temperature")
-    outside_temperature = self.get_state(self.entity_id_weather_forecast, attribute="temperature")
-    # self.log(f"Inside temperature: {inside_temperature}, Outside temperature: {outside_temperature}")
 
     current_hour = datetime.now().hour
     hourly_prices = self.calculate_hourly_prices()
@@ -133,7 +329,7 @@ class ACController(hass.Hass):
     if (price_now < 0):
       price_now = 0
     self.log(f"Price now: {price_now}, Mean price {mean_price}")
-    self.log(f"Today+tomorrow full prices {len(hourly_prices)} items: {hourly_prices}")
+    # self.log(f"Today+tomorrow full prices {len(hourly_prices)} items: {hourly_prices}")
 
     if (inside_temperature < self.target_room_min_temperature):
       return self.target_room_min_temperature
@@ -183,7 +379,7 @@ class ACController(hass.Hass):
     self.log(f"Time since last state change: {datetime.now() - self.last_state_change_time}")
 
     if ( (datetime.now() - self.last_state_change_time) < timedelta(seconds=self.min_state_change_time) ):
-      if ( (abs(inside_temperature - target_temperature)) < self.ignore_change_time_temp_diff ):
+      if ( (abs(ac_current_target_temperature - target_temperature)) < self.ignore_change_time_temp_diff ):
         # Not yet time for a scheduled state change and the temperature deviation is not large enough, don't do anything
         return
 
